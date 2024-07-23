e8fad434b0d95756afc82f0a951568860a885c6c
sjin <sjin@konkuk.ac.kr>
2024-07-23 21:15:51 +0900

    24.07.23

--- Baekjoon/11501.cpp
+++ Baekjoon/11501.cpp
@@ -3,29 +3,26 @@
 #include <algorithm>
 using namespace std;
 
+typedef long long ll;
+
 int main(){
-    int T;
+    ll T;
     cin>>T;
 
     while(T--){
-        int N,profit=0;
+        ll N,profit=0;
         cin>>N;
 
-        vector<int> Price(N,0),Price_sorted(N,0);
-        for(int i=0;i<N;++i){
-            cin>>Price[i];
-            Price_sorted[i]=Price[i];
+        vector<ll> Price(N,0),Price_sorted(N,0),Max(N,-1);
+        for(ll i=0;i<N;++i)cin>>Price[i];
+
+        for(ll i=N-2;i>=0;--i){
+            if(Price[i+1]>Max[i+1]) Max[i]=Price[i+1];
+            else Max[i]=Max[i+1];
         }
-        sort(Price_sorted.begin(),Price_sorted.end(),greater<int>());
-        int idx=0;
-
-        for(int i=0;i<N;++i){
-            int cur=Price[i];
-
-            if(idx<N&&cur<Price_sorted[idx]) profit+=Price_sorted[idx]-cur;
-            else if(cur==Price_sorted[idx]) idx++;
+        for(ll i=0;i<N;++i){
+            if(Max[i]>Price[i]) profit+=Max[i]-Price[i];
         }
-
         cout<<profit<<"\n";
     }
     return 0;
